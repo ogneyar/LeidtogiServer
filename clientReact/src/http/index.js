31e2f224c29a6149a479c9da6bb2365f9cda224c
ogneyar <ogneyar-ne@ya.ru>
2021-07-02 18:04:21 +0300

    add cross-env

--- clientReact/src/http/index.js
+++ clientReact/src/http/index.js
@@ -1,11 +1,21 @@
 import axios from 'axios'
 
+let API_URL
+
+if (process.env.REACT_APP_ENV === 'production') {
+    API_URL = process.env.REACT_APP_API_URL_PRODUCTION
+}else if (process.env.REACT_APP_ENV === 'develop') {
+    console.log("development mode run")
+    API_URL = process.env.REACT_APP_API_URL
+}else console.log("NOTE USE CROSS-ENV")
+
+
 const $host = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: API_URL
 })
 
 const $authHost = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: API_URL
 })
 
 const authInterceptor = config => {
