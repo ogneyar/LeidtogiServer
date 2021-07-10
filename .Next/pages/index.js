0d802fca66bff0ca17b96d84f9112abf51159b63
ogneyar <ogneyar-ne@ya.ru>
2021-07-10 22:55:57 +0300

    лишнее

--- .Next/pages/index.js
+++
@@ -1,6 +0,0 @@
-import App from '../components/App'
-import styles from '../styles/Index.module.css'
-
-export default function Index() {
-  return <App />
-}
--- .Next/utils/consts.js
+++
@@ -1,9 +0,0 @@
-export const ADMIN_ROUTE = '/admin'
-export const LOGIN_ROUTE = '/login'
-export const REGISTRATION_ROUTE = '/registration'
-export const SHOP_ROUTE = '/'
-export const BASKET_ROUTE = '/basket'
-export const DEVICE_ROUTE = '/device'
-
-export const API_URL = "http://localhost:5000/"  // server
-export const HOST_URL = "http://localhost:3000/" // client
