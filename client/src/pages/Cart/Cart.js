363ba50ed4074d5dc0da8e4b763697d42ef972cf
ogneyar <ogneyar-ne@ya.ru>
2021-08-17 18:12:55 +0300

    Доработал корзину покупателя

--- client/src/pages/Cart/Cart.js
+++ client/src/pages/Cart/Cart.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Card, Row, Col } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
-import { SHOP_ROUTE } from '../../utils/consts'
+import { SHOP_ROUTE, API_URL } from '../../utils/consts'
 import { Container, Button } from '../../components/myBootstrap'
 import './Cart.css'
 
@@ -9,37 +9,56 @@ import './Cart.css'
 const Cart = () => {
 
     const [state, setState] = useState([])
+    const [total, setTotal] = useState(0)
 
     let cart
 
     useEffect(() => {
         cart = localStorage.getItem('cart')
-        setState(JSON.parse(cart))
+        cart = JSON.parse(cart)
+        setState(cart)
+        let totalValue = 0
+        cart.forEach(i => totalValue += i.total)
+        setTotal(totalValue)
     }, [])
 
     const onClickButtonPlus = (record) => {
         cart = localStorage.getItem('cart')
         cart = JSON.parse(cart)
+        let totalValue = 0
         cart = cart.map(i => {
             if (i.id === record.id) {
-                return {...i, value:i.value+1}
-            }else return i
+                let newValue = i.value + 1
+                let newTotal = newValue * i.price
+                totalValue += newTotal
+                return {...i, value: newValue, total: newTotal}
+            }
+            totalValue += i.total
+            return i
         })
         setState(cart)
+        setTotal(totalValue)
         localStorage.setItem('cart', JSON.stringify(cart))
     }
 
     const onClickButtonMinus = (record) => {
         cart = localStorage.getItem('cart')
         cart = JSON.parse(cart)
+        let totalValue = 0
         cart = cart.map(i => {
             if (i.id === record.id) {
-                if (i.value > 1)
-                    return {...i, value:i.value-1}
-                else return i
-            }else return i
+                if (i.value > 1) {
+                    let newValue = i.value - 1
+                    let newTotal = newValue * i.price
+                    totalValue += newTotal
+                    return {...i, value: newValue, total: newTotal}
+                }
+            }
+            totalValue += i.total
+            return i
         })
         setState(cart)
+        setTotal(totalValue)
         localStorage.setItem('cart', JSON.stringify(cart))
     }
     
@@ -49,15 +68,23 @@ const Cart = () => {
         if (cart.length === 1) {
             localStorage.removeItem('cart')
             setState(null)
+            setTotal(0)
         }else {
-            cart = cart.filter(i => i.id !== record.id)
+            let totalValue = 0
+            cart = cart.filter(i => {
+                if (i.id !== record.id) {
+                    totalValue += i.total
+                    return true
+                }
+                return false
+            })
             setState(cart)
+            setTotal(totalValue)
             localStorage.setItem('cart', JSON.stringify(cart))
         }
     }
 
     if (state) {
-        // localStorage.removeItem('cart')
 
         return (
             <Container 
@@ -73,70 +100,108 @@ const Cart = () => {
                     <table>
                         <thead>
                             <tr>
-                                <th>
-                                    Номер:
-                                </th>
-                                <th>
-                                    Наименование товара:
-                                </th>
-                                <th>
-                                    Артикул:
-                                </th>
-                                <th>
-                                    Количество:
-                                </th>
+                                <th>№:</th>
+                                <th>Наим. товара:</th>
+                                <th>Фото:</th>
+                                <th>Цена:</th>
+                                <th>Кол-во:</th>
+                                <th>Итого:</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {Array.isArray(state) && state.map(i => {
+                            {Array.isArray(state) && state.map((i,index) => {
                                 return (
                                     <tr
                                         key={i.id}
                                     >
                                         <th>
-                                            {i.id}
-                                        </th>
-                                        <th>
-                                            {i.name}
-                                        </th>
-                                        <th>
-                                            {i.article}
-                                        </th>
-                                        <th>
-                                            <Button
-                                                // className="pl-2 pr-2"
-                                                style={{width:"30px"}}
-                                                onClick={() => onClickButtonMinus(i)}
+                                            <div
+                                                className="CartThDivRow"
                                             >
-                                                -
-                                            </Button>
+                                                {index+1}
+                                            </div>
+                                        </th>
+                                        <th>
+                                            <div
+                                                className="CartThDivRow"
+                                            >
+                                                {i.name}
+                                            </div>
+                                        </th>
+                                        <th>
+                                            <div
+                                                className="CartThDivRow"
+                                            >
+                                                <img src={API_URL + i.img} width="50" height="50" alt={i.name} />
+                                            </div>
+                                        </th>
+                                        <th>
+                                            <div
+                                                className="CartThDivRow"
+                                            >
+                                                {i.price}
+                                            </div>
+                                        </th>
+                                        <th>
+                                            <div
+                                                className="d-flex flex-column"
+                                            >
+                                                <div
+                                                    className="CartThDivRow"
+                                                >
+                                                    <Button
+                                                        className="CartButtonValue"
+                                                        onClick={() => onClickButtonMinus(i)}
+                                                    >
+                                                        -
+                                                    </Button>
+                                                    <div
+                                                        className="CartValue"
+                                                    >
+                                                        {i.value}
+                                                    </div>
 
-                                            {/* <div
-                                                className="pl-2 pr-2"
-                                            > */}
-                                                &nbsp;&nbsp;
-                                                {i.value}
-                                                &nbsp;
-                                            {/* </div> */}
-
-                                            <Button
-                                                // className="pl-2 pr-2"
-                                                style={{width:"30px"}}
-                                                onClick={() => onClickButtonPlus(i)}
-                                            >
-                                                +
+                                                    <Button
+                                                        className="CartButtonValue"
+                                                        onClick={() => onClickButtonPlus(i)}
+                                                    >
+                                                        +
+                                                    </Button>
+                                                </div>
+                                                <Button
+                                                    className="CartButtonDelete"
+                                                    onClick={() => onClickButtonDelete(i)}
+                                                    variant="danger"
+                                                >
+                                                    Удалить
                                             </Button>
-                                            <Button
-                                                className="ml-4"
-                                                onClick={() => onClickButtonDelete(i)}
-                                                variant="danger"
+                                            </div>
+                                        </th>
+                                        <th>
+                                            <div
+                                                className="CartThDivRow"
                                             >
-                                                Удалить
-                                            </Button>
+                                                {i.total}
+                                            </div>
                                         </th>
                                     </tr>
                                 )
                             })}
+                            <tr>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                                <th>Итого:</th>
+                                <th>
+                                    <div
+                                        className="CartThDivRow"
+                                    >
+                                        {total}
+                                    </div>
+                                    
+                                </th>
+                            </tr>
                         </tbody>
                     </table>
 
--- client/src/service/cart/CartBuyService.js
+++ client/src/service/cart/CartBuyService.js
@@ -11,17 +11,32 @@ export const onClickButtonBuy = (e, product) => {
         cart = cart.map(i => {
             if (i.id === product.id) {
                 yes = "yes"
-                // let number = i.value
-                // number++
-                return {...i, value:i.value+1}
+                let newValue = i.value + 1
+                return {...i, value: newValue, total: i.price * newValue}
             } else return i
         })
         if (!yes) {
-            cart = [...cart, {id:product.id,value:1,name:product.name,article:null}]
+            cart = [...cart, {
+                id: product.id,
+                value: 1,
+                name: product.name,
+                article: product.article,
+                price: product.price,
+                img: product.img,
+                total: product.price
+            }]
         }
         
     }else {
-        cart = [{id:product.id,value:1,name:product.name,article:null}]
+        cart = [{
+            id:product.id,
+            value:1,
+            name:product.name,
+            article:product.article,
+            price:product.price,
+            img:product.img,
+            total: product.price
+        }]
     }
     localStorage.setItem('cart', JSON.stringify(cart))
     // alert("Товар добавлен в корзину.")
