c46e3bb462caebf742f19beb7942231fb8a50c5d
ogneyar <ogneyar-ne@ya.ru>
2021-08-18 09:30:25 +0300

    файлы для загрузки выгрузки данных

--- dump.bat
+++ dump.bat
@@ -1,5 +1,7 @@
 @echo off
 
+rem скачивание dump'a БД mern
+
 "C:/Program Files/MySQL/MySQL Server 5.5/bin/mysqldump" --host=127.0.0.1 --password= -u root --disable-keys --add-drop-table --default-character-set=utf8 --result-file=mern.sql mern
 
 echo =================SUCCESS========================
--- load_dump.bat
+++ load_dump.bat
@@ -1,5 +1,7 @@
 @echo off
 
+rem загрузка БД mern в MySQL
+
 "C:/Program Files/MySQL/MySQL Server 5.5/bin/mysql" --host=127.0.0.1 --password= -u root --default-character-set=utf8 mern < mern.sql
 
 echo ================SUCCESS=========================
