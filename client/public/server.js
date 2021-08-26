de2efd4c5993319f99033923c0348955f82edd8c
ogneyar <ogneyar-ne@ya.ru>
2021-08-26 11:23:27 +0300

    сделал загрузочную страницу при обновлении сайта

--- client/public/server.js
+++ client/public/server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var	port = 80;
+const fs = require('fs')
+
+fs.stat("upgrade", (err, stats) => {
+    if (err) {
+        express()
+            .get('*', (req, res) => res.sendFile(__dirname + '/index.html'))
+            .listen(port, () => console.log(`Starting server. http://localhost:${ port }`))
+    }else {
+        express()
+            .get('*', (req, res) => res.sendFile(__dirname + '/upgrade.html'))
+            .listen(port, () => console.log(`Starting server. http://localhost:${ port }`))
+    }
+})
 	
-express()
-    // .use(express.static('static'))
-    .get('*', (req, res) => res.sendFile(__dirname + '/index.html'))
-    .listen(port, () => console.log(`Starting server. http://localhost:${ port }`));
