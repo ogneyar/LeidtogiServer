e412bb476fe0e458bd1fb516de6d6fbbfc57bd85
ogneyar <ogneyar-ne@ya.ru>
2021-08-26 12:19:04 +0300

    мелкие правки

--- client/public/server.js
+++ client/public/server.js
@@ -2,15 +2,15 @@ var express = require('express');
 var	port = 80;
 const fs = require('fs')
 
+const app = express()
+
 fs.stat("upgrade", (err, stats) => {
     if (err) {
-        express()
-            .get('*', (req, res) => res.sendFile(__dirname + '/index.html'))
-            .listen(port, () => console.log(`Starting server. http://localhost:${ port }`))
+        app.get('*', (req, res) => res.sendFile(__dirname + '/index.html'))
     }else {
-        express()
-            .get('*', (req, res) => res.sendFile(__dirname + '/upgrade.html'))
-            .listen(port, () => console.log(`Starting server. http://localhost:${ port }`))
+        app.get('*', (req, res) => res.sendFile(__dirname + '/upgrade_index.html'))
+            
     }
 })
 	
+app.listen(port, () => console.log(`Starting server. http://localhost:${ port }`))
