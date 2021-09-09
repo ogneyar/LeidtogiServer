787dcbcab351502e824a4a3f0de2f3766d2bd795
ogneyar <ogneyar-ne@ya.ru>
2021-09-09 14:20:42 +0300

    поправил пагинацию

--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -1,6 +1,7 @@
+import React, { useContext, useState } from 'react'
 import { observer } from 'mobx-react-lite'
-import React, { useContext } from 'react'
 import { Pagination } from 'react-bootstrap'
+import uuid from 'react-uuid'
 import $ from 'jquery'
 
 import { Context } from '..'
@@ -10,6 +11,9 @@ const Pages = observer(() => {
 
     const { product } = useContext(Context)
 
+    // const [ doted, setDoted ] = useState(false)
+    let doted = false
+
     const pageCount = Math.ceil(product.totalCount / product.limit)
     const pages = []
 
@@ -17,36 +21,137 @@ const Pages = observer(() => {
         pages.push(i + 1)
     }
 
+    const onClick = (page) => {
+        if (window.innerWidth > 991) {
+            $('html, body').animate(
+                {scrollTop: 240}, 
+                700, 
+                function(){}
+            )
+        }else {
+            $('html, body').animate(
+                {scrollTop: 130}, 
+                700, 
+                function(){}
+            )
+        }
+        product.setPage(page)
+    }
+
     return (
         <Pagination className="mt-2">
-            {/* {product.totalCount > product.limit ? <Pagination.First /> : ""}
-            {product.totalCount > product.limit ? <Pagination.Prev /> : ""}             */}
+            {pages.length > 10 
+            ? 
+                <Pagination.First
+                    onClick={() =>{product.page !== 1 && onClick(1)}}
+                    disabled={product.page === 1 && true}
+                /> 
+            : null}
+            {pages.length > 10 
+            ? 
+                <Pagination.Prev
+                    onClick={() =>{product.page !== 1 && onClick(product.page - 1)}}
+                    disabled={product.page === 1 && true}
+                /> 
+            : null}
             {pages.map(page => 
-                <Pagination.Item
-                    key={page}
-                    active={product.page === page}
-                    onClick={() => {
-                        if (window.innerWidth > 991) {
-                            $('html, body').animate(
-                                {scrollTop: 240}, 
-                                700, 
-                                function(){}
-                            )
-                        }else {
-                            $('html, body').animate(
-                                {scrollTop: 130}, 
-                                700, 
-                                function(){}
-                            )
-                        }
-                        product.setPage(page)
-                    }}
-                >
-                    {page}
-                </Pagination.Item>
+                (
+                pages.length > 10 
+                ? 
+                    product.page < 5 // если выбраная страница от 1 до 4
+                    ?
+                        page < 8 // то показывать первые 7 кнопок
+                        ?
+                            <Pagination.Item
+                                key={page}
+                                active={product.page === page}
+                                onClick={() => onClick(page)}
+                            >
+                                {page}
+                            </Pagination.Item>
+                        : 
+                            !doted // если ещё небыло показано троеточие
+                            ? // то показать
+                                <Pagination.Item
+                                    key={page}
+                                    disabled
+                                    >
+                                    ...
+                                    {doted = true}
+                                </Pagination.Item>
+                            : null
+                    :
+                        product.page >= pages.length - 3 // если выбраная страница от конца 4ая, 3ья, предпоследняя или последняя
+                        ?
+                            page > pages.length - 7 // то показывать последние 7 кнопок
+                            ?
+                                <Pagination.Item
+                                    key={page}
+                                    active={product.page === page}
+                                    onClick={() => onClick(page)}
+                                >
+                                    {page}
+                                </Pagination.Item>
+                            : 
+                                !doted // если ещё небыло показано троеточие
+                                ? // то показать
+                                    <Pagination.Item
+                                        key={uuid()}
+                                        disabled
+                                        >
+                                        ...
+                                        {doted = true}
+                                    </Pagination.Item>
+                                : null
+
+                        : // иначе, если выбраная страница где-то в середине
+                            page <= product.page + 3 && page >= product.page - 3 // то показать 7 штук
+                            ?
+                                <Pagination.Item
+                                    key={page}
+                                    active={product.page === page}
+                                    onClick={() => onClick(page)}
+                                >
+                                    {page}
+                                    {doted = false}
+                                </Pagination.Item>
+                            : 
+                                !doted // если ещё небыло показано троеточие
+                                ? // то показать
+                                    <Pagination.Item
+                                        key={page}
+                                        disabled
+                                        >
+                                        ...
+                                        {doted = true}
+                                    </Pagination.Item>
+                                : null
+
+                :
+                    <Pagination.Item
+                        key={page}
+                        active={product.page === page}
+                        onClick={() => onClick(page)}
+                    >
+                        {page}
+                    </Pagination.Item>
+               
+                )
             )}
-            {/* {product.totalCount > product.limit ? <Pagination.Next /> : ""}
-            {product.totalCount > product.limit ? <Pagination.Last /> : ""} */}
+            {pages.length > 10 
+            ? 
+                <Pagination.Next
+                    onClick={() =>{product.page !== pages.length && onClick(product.page + 1)}}
+                    disabled={product.page === pages.length && true}
+                /> 
+            : null}
+            {pages.length > 10 
+            ? 
+                <Pagination.Last
+                    onClick={() =>{product.page !== pages.length && onClick(pages.length)}}
+                    disabled={product.page === pages.length && true}
+                /> 
+            : null}
         </Pagination>
     )
 })
