9ba4b981e6e61561b0be1299d702c18915a0bc97
ogneyar <ogneyar-ne@ya.ru>
2021-12-04 13:23:07 +0300

    add ssl

--- server/service/mailService.js
+++ server/service/mailService.js
@@ -5,8 +5,10 @@ class MailService {
     constructor() {
         this.transporter = nodemailer.createTransport({
             host: process.env.SMTP_HOST,
-            port: process.env.SMTP_PORT,
-            secure: false,
+            port: process.env.SMTP_PORT_SECURE,
+            // port: process.env.SMTP_PORT,
+            // secure: false,
+            secure: true,
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASSWORD
@@ -19,13 +21,13 @@ class MailService {
             let response = await this.transporter.sendMail({
                 from: process.env.SMTP_USER,
                 to,
-                subject: 'Активация аккаунта на ' + process.env.CORS_URL,
+                subject: 'Активация аккаунта на ' + process.env.CORS_URL_SECURE,
                 text: '',
                 html:
                     `
                         <div>
                             <h1>Для активации перейдите по ссылке</h1>
-                            <a href="${process.env.CORS_URL}/confirmation/${link}">${process.env.CORS_URL}/confirmation/${link}</a>
+                            <a href="${process.env.CORS_URL_SECURE}/confirmation/${link}">${process.env.CORS_URL_SECURE}/confirmation/${link}</a>
                         </div>
                     `
             })
