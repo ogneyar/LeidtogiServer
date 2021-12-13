d1bdef4809ceff8c3b2feaae769f1311a5316b02
ogneyar <ogneyar-ne@ya.ru>
2021-12-13 12:04:33 +0300

    контроллер для доставщика boxberry

---
+++ server/controllers/delivery/boxberryController.js
@@ -0,0 +1,53 @@
+
+const { Delivery } = require('../../models/models')
+const Boxberry = require("../../service/delivery/boxberry/Boxberry")
+
+
+class BoxberryController {
+
+    async deliveryCosts(req, res) {
+        // return res.json(await Boxberry.deliveryCosts(req.body))
+        return res.json(await Boxberry.deliveryCosts(req.query))
+    }
+
+    async listCities(req, res) {
+        // let { CountryCode, Name, Region } = req.body
+        let { CountryCode, Name, Region } = req.query
+        let response = await Boxberry.listCities({CountryCode})
+        if (response.error === undefined) {
+            if (Name) return res.json(response.filter(i => i.Name.includes(Name)))
+            if (Region) return res.json(response.filter(i => i.Region.includes(Region)))
+        }
+        return res.json(response)
+    }
+
+    async listCitiesFull(req, res) {
+        // return res.json(await Boxberry.listCitiesFull(req.body))
+        return res.json(await Boxberry.listCitiesFull(req.query))
+    }
+
+    async listPoints(req, res) {
+        // return res.json(await Boxberry.listPoints(req.body))
+        return res.json(await Boxberry.listPoints(req.query))
+    }
+
+    async listZips(req, res) { // Список почтовых индексов для КД (Курьерская Доставка)
+        // let { City, Region } = req.body
+        let { City, Region } = req.query
+        let response = await Boxberry.listZips()
+        if (response.error === undefined) {
+            if (City) return res.json(response.filter(i => i.City.includes(City)))
+            if (Region) return res.json(response.filter(i => i.Region.includes(Region)))
+        }
+        return res.json(response)
+    }
+
+    async zipCheck(req, res) { // Проверка почтового индекса для КД (Курьерская Доставка)
+        // let response = await Boxberry.zipCheck(req.body)
+        let response = await Boxberry.zipCheck(req.query)
+        if (response.err !== undefined) response = {error: response.err}
+        return res.json(response)
+    }
+}
+
+module.exports = new BoxberryController()
