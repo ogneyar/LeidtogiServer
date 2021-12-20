a0061544e74dc5784aae6edb4df8babf6d0ec0e1
ogneyar <ogneyar-ne@ya.ru>
2021-12-20 22:10:39 +0300

    исправил фид и добавил категории

--- server/controllers/testerController.js
+++ server/controllers/testerController.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 
-const {Product, Brand} = require('../models/models')
+const {Product, Brand, Category} = require('../models/models')
 
 
 class TesterController {
@@ -10,32 +10,61 @@ class TesterController {
         try {
             const products = await Product.findAll()
             const brands = await Brand.findAll()
+            const categories = await Category.findAll()
 
-            const xml = `<yml_catalog>
-    <offers>${products.map(i => {
-        let brandName = brands
-            .filter(j => j.id === i.brandId)
-            .map(k => k.name.toLowerCase())
-            .join("")
-        return (`
-        <offer id="${i.article}" available="${i.have === 1 ? "true" : "false"}">
-            <name>${i.name}</name>
-            <url>https://leidtogi.ru/${brandName}/${i.url}</url>
-            <picture>https://server.leidtogi.ru/${JSON.parse(i.img)[0].big}</picture>
-            <price>${i.price}</price>
-            <currencyId>RUB</currencyId>
-            <categoryId>${i.categoryId}</categoryId>
-            <pickup>true</pickup>
-            <delivery>true</delivery>
-            <manufacturer_warranty>true</manufacturer_warranty>
-            <country_of_origin>${i.country}</country_of_origin>
-        </offer>`)
-    }).join("")}
-    </offers>
+            let date = new Date().toISOString()
+            let formatDate = date.substring(0, date.indexOf("."))
+            // <name>${i.name.include(/"/g,"&quot;").include(/&/g,"&amp;").include(/>/g,"&gt;").include(/</g,"&lt;").include(/'/g,"&apos;")}</name>
+
+            const xml = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<yml_catalog date="${formatDate}">
+    <shop>
+        <name>ЛЕИДТОГИ</name>
+        <company>ООО &quot;ЛЕИДТОГИ&quot;</company>
+        <url>https://leidtogi.ru</url>
+        <currencies>
+            <currency id="RUR" rate="1"/>
+        </currencies>
+        <categories>${categories.map(i => {
+            if (i.sub_category_id === 0) {
+            return (`
+            <category id="${i.id}">${i.name}</category>`)
+            }
+            return (`
+            <category id="${i.id}" parentId="${i.sub_category_id}">${i.name}</category>`)
+        }).join("")}
+        </categories>
+        <offers>${products.map(i => {
+            let brandName = brands
+                .filter(j => j.id === i.brandId)
+                .map(k => k.name.toLowerCase())
+                .join("")
+            let name = i.name
+                .replace(/"/g,"&quot;")
+                .replace(/&/g,"&amp;")
+                .replace(/>/g,"&gt;")
+                .replace(/</g,"&lt;")
+                .replace(/'/g,"&apos;")
+            return (`
+            <offer id="${i.article}" available="${i.have === 1 ? "true" : "false"}">
+                <name>${name}</name>
+                <url>https://leidtogi.ru/${brandName}/${i.url}</url>
+                <picture>https://server.leidtogi.ru/${JSON.parse(i.img)[0].big}</picture>
+                <price>${i.price}</price>
+                <currencyId>RUB</currencyId>
+                <categoryId>${i.categoryId}</categoryId>
+                <pickup>true</pickup>
+                <delivery>true</delivery>
+                <manufacturer_warranty>true</manufacturer_warranty>
+                <country_of_origin>${i.country}</country_of_origin>
+            </offer>`)
+        }).join("")}
+        </offers>
+    </shop>
 </yml_catalog>`
 
             fs.writeFileSync(path.resolve(__dirname, '..', 'static', 'feed', 'yml.xml'), xml)
-            
+
             return res.json(true)
         }catch(e) {
             return  res.json({error:'Ошибка метода setFeed!'})
