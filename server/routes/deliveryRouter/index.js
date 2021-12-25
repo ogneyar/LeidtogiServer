2c7ccca0a5eafd52c1e3e816b94a3b3331d7c480
ogneyar <ogneyar-ne@ya.ru>
2021-12-25 17:22:48 +0300

    мелкие правки

--- server/routes/deliveryRouter/index.js
+++ server/routes/deliveryRouter/index.js
@@ -1,14 +1,16 @@
 const Router = require('express')
 const router = new Router()
 
-const prRouter = require('./prRouter')
 const sdekRouter = require('./sdekRouter')
 const boxberryRouter = require('./boxberryRouter')
+const dlRouter = require('./dlRouter')
+// const pekRouter = require('./pekRouter')
+// const prRouter = require('./prRouter')
 
-router.use('/pr', prRouter) // Почта России
 router.use('/sdek', sdekRouter) // СДЭК
-// router.use('/dl', dlRouter) // Деловые Линии
-// router.use('/pek', pekRouter) // ПЭК
 router.use('/boxberry', boxberryRouter) // БоксБери
+router.use('/dl', dlRouter) // Деловые Линии
+// router.use('/pek', pekRouter) // ПЭК
+// router.use('/pr', prRouter) // Почта России
 
 module.exports = router
