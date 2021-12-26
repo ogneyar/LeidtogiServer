5edad84987f99f7adfef801a601c67f5ba2717da
ogneyar <ogneyar-ne@ya.ru>
2021-12-26 17:12:56 +0300

    добавил ещё роутов

--- server/routes/deliveryRouter/dlRouter.js
+++ server/routes/deliveryRouter/dlRouter.js
@@ -8,7 +8,12 @@ const authMiddlewarre = require('../../middleware/authMiddleware')
 router.post('/auth', authMiddlewarre, dlController.auth) // авторизация
 // router.get('/auth', dlController.auth) // авторизация
 router.get('/calculator', dlController.calculator) // Калькулятор стоимости и сроков заказа
-router.get('/micro_calc', dlController.micro_calc) // Калькулятор ориентировочной стоимости и сроков заказа
+router.get('/micro_calc', dlController.microCalc) // Калькулятор ориентировочной стоимости и сроков заказа
+
+router.get('/kladr', dlController.kladr) // Поиск населённых пунктов
+router.get('/url_terminals', dlController.terminals) // Справочник терминалов
+router.get('/terminals_catalog', dlController.terminalsCatalog) // Справочник терминалов
+router.get('/search_terminals', dlController.searchTerminals) // Поиск терминалов
 
 
 module.exports = router
