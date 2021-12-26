fcacb3036b881d26c82ca5fb13c97127a658bc8c
ogneyar <ogneyar-ne@ya.ru>
2021-12-26 17:13:27 +0300

    добавил методов

--- server/controllers/delivery/dlController.js
+++ server/controllers/delivery/dlController.js
@@ -4,18 +4,33 @@ const Dl = require("../../service/delivery/dl/Dl")
 
 class DlController {
 
-    async auth(req, res) {
+    async auth(req, res) { // авторизация
         return res.json(await Dl.auth())
     }
 
-    async calculator(req, res) {
+    async calculator(req, res) { // Калькулятор стоимости и сроков заказа
         return res.json(await Dl.calculator(req.query))
     }
 
-    async micro_calc(req, res) {
-        return res.json(await Dl.micro_calc(req.query))
+    async microCalc(req, res) { // Калькулятор ориентировочной стоимости и сроков заказа
+        return res.json(await Dl.microCalc(req.query))
     }
 
+    async kladr(req, res) { // Поиск населённых пунктов
+        return res.json(await Dl.kladr(req.query))
+    }
+
+    async terminals(req, res) { // Справочник терминалов
+        return res.json(await Dl.terminals())
+    }
+
+    async terminalsCatalog(req, res) { // Справочник терминалов
+        return res.json(await Dl.terminalsCatalog(req.query))
+    }
+
+    async searchTerminals(req, res) { // Поиск терминалов
+        return res.json(await Dl.searchTerminals(req.query))
+    }
 
 }
 
