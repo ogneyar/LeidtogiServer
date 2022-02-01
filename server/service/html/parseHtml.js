d2fe313aa0e3f9a1f20780b334ffb104ad90ba55
ogneyar <ogneyar-ne@ya.ru>
2022-02-01 18:30:20 +0300

    создал парсер html файлов

---
+++ server/service/html/parseHtml.js
@@ -0,0 +1,71 @@
+//
+// args = { entry: `искомая строка` } // return true or false
+//
+// args = {
+//      entry: `<div class="class_name">`, // entryPoint // точка входа (от куда начинать поиск)
+//      start: `href="`, // startSearch //  стартовая строка
+//      end: `"`, // endSearch //  финишная строка
+//      inclusive: false, // inclusive or excluded start and end search string // включительно или исключая стартовую и финишную строки
+//      return: false // return the rest of the string // вернуть остальную строку
+// }
+//
+function parseHtml(string, args) { 
+
+    let entryPoint = null, startSearch = undefined, endSearch = undefined, inclusive = false, returnString = false
+
+    if ( ! args.entry && ! args.start && ! args.end ) throw `Нет необходимых параметров! (parseHtml)`
+    if (args.entry !== undefined) entryPoint = args.entry // точка входа (от куда начинать поиск)
+    if (args.start !== undefined) startSearch = args.start //  стартовая строка
+    if (args.end !== undefined) endSearch = args.end //  финишная строка
+    if (args.inclusive !== undefined) inclusive = args.inclusive // включительно или исключая стартовую и финишную строки
+    if (args.return !== undefined) returnString = args.return // включительно или исключая стартовую и финишную строки
+
+    let number, rest
+
+    if (entryPoint) { 
+        number = string.indexOf(entryPoint)
+        if (number === -1) {
+            if ( ! startSearch && ! endSearch) return false
+            else throw `Не найден '${entryPoint}'! (parseHtml)`
+        }else {
+            if ( ! startSearch && ! endSearch) return true
+        }
+        string = string.substring(number + entryPoint.length, string.length)
+        if (!string) throw `Не сработал substring после найденого '${entryPoint}'! (parseHtml)`
+
+        
+    }
+
+    number = string.indexOf(startSearch)
+    if (number === -1) throw `Не найден '${startSearch}'! (parseHtml)`
+    if (inclusive) string = string.substring(number, string.length)
+    else string = string.substring(number + startSearch.length, string.length)
+    if (!string) throw `Не сработал substring после найденого '${startSearch}'! (parseHtml)`
+    
+    number = string.indexOf(endSearch)
+    if (number === -1) throw `Не найден '${endSearch}'! (parseHtml)`
+    // if (returnString) rest = string
+    if (inclusive) {
+        if (returnString) rest = string.substring(number + endSearch.length, string.length)
+        string = string.substring(0, number + endSearch.length)
+    }else {
+        if (returnString) rest = string.substring(number, string.length)
+        string = string.substring(0, number)
+    }
+    if (!string) throw `Не сработал substring после найденого '${endSearch}'! (parseHtml)`
+
+    string = string
+        .replace(/\r|\n|\t/g,"")
+        .replace(/(&nbsp;)/g," ")
+
+    if (returnString) {
+        rest = rest.replace(/\r|\n|\t/g,"")
+
+        return { rest, search: string }
+    }
+
+    return string
+
+}
+
+module.exports = parseHtml
