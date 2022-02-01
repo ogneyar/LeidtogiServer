4a6017282ee159b7b210060d5eef791c59842773
ogneyar <ogneyar-ne@ya.ru>
2022-02-01 18:29:19 +0300

    убрал лишние роуты

--- server/routes/parserRouter/husqvarnaRouter.js
+++ server/routes/parserRouter/husqvarnaRouter.js
@@ -7,11 +7,7 @@ const checkRole = require('../../middleware/checkRoleMiddleware')
 // router.get('/', husqvarnaController.husqvarna) // добавление нового товара или обновление цен
 router.post('/', checkRole("ADMIN"), husqvarnaController.husqvarna) // добавление нового товара или обновление цен
 
-router.get('/get_image', husqvarnaController.getImage) // получение изображения товара
-router.get('/get_charcteristics', husqvarnaController.getCharcteristics) // получение характеристик товара
-router.get('/get_description', husqvarnaController.getDescription) // получение описания товара
-
-router.get('/test', husqvarnaController.test) // тест
+router.get('/test', husqvarnaController.test) // тест 
 
 
 module.exports = router
