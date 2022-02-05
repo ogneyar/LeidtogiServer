93a6d7e02a342020daa413e70251c0644beeb9c6
ogneyar <ogneyar-ne@ya.ru>
2022-02-05 18:26:02 +0300

    добавил роутов

--- server/routes/deliveryRouter/dlRouter.js
+++ server/routes/deliveryRouter/dlRouter.js
@@ -3,6 +3,7 @@ const router = new Router()
 
 const dlController = require('../../controllers/delivery/dlController')
 const authMiddlewarre = require('../../middleware/authMiddleware')
+const checkRole = require('../../middleware/checkRoleMiddleware')
 
 
 router.post('/auth', authMiddlewarre, dlController.auth) // авторизация
@@ -14,6 +15,18 @@ router.get('/kladr', dlController.kladr) // Поиск населённых пу
 router.get('/url_terminals', dlController.terminals) // Справочник терминалов
 router.get('/terminals_catalog', dlController.terminalsCatalog) // Справочник терминалов
 router.get('/search_terminals', dlController.searchTerminals) // Поиск терминалов
+
+router.get('/request_conditions', dlController.requestConditions) // Ограничения по параметрам заказа
+
+// этот роут находится в testerRouter
+// router.get('/places', checkRole("ADMIN"), dlController.places) // Справочник населённых пунктов
+// этот роут пока неподключен
+// router.get('/streets', checkRole("ADMIN"), dlController.streets) // Справочник улиц
+// этот роут пока неподключен
+// router.get('/load_types', checkRole("ADMIN"), dlController.loadTypes) // Справочник видов загрузки
+// этот роут пока неподключен
+// router.get('/servises', checkRole("ADMIN"), dlController.servises) // Справочник специальных требований к транспорту
+
 
 
 module.exports = router
