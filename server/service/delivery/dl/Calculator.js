3de84de0013f7bf31afa76501481e8020fee7c7a
ogneyar <ogneyar-ne@ya.ru>
2022-02-05 18:25:36 +0300

    доделал полноценный подсчёт стоимости доставки товаров

--- server/service/delivery/dl/Calculator.js
+++ server/service/delivery/dl/Calculator.js
@@ -1,4 +1,7 @@
 
+const Delivery = require("./classes/Delivery")
+const Cargo = require("./classes/Cargo")
+
 
 module.exports = class Calculator {
     
@@ -6,13 +9,21 @@ module.exports = class Calculator {
     method // метод для доступа к api-сервисам
     
     // -------------------
-    // необязательные поля
+    // обязательные поля
     // -------------------
+
+    delivery // Информация по перевозке груза
+    cargo // Информация о грузе
+    // в данном случае если cargo не передавать, то ошибки не произойдёт
 
     
     constructor(parameters) {
         this.appkey = process.env.DL_APPKEY
         this.method = "v2/calculator.json"
 
+        if (parameters.delivery === undefined) throw "Отсутствует обязательный параметр delivery."
+        this.delivery = new Delivery(parameters.delivery)
+
+        this.cargo = new Cargo(parameters.cargo)
     }
 }
