d598b8d3229a5e2c31cbe78c344f7958221eeb71
ogneyar <ogneyar-ne@ya.ru>
2022-02-06 16:49:41 +0300

    добавил роут get_one

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -5,6 +5,7 @@ const checkRole = require('../middleware/checkRoleMiddleware')
 
 router.post('/', checkRole('ADMIN'), brandController.create)
 router.get('/', brandController.getAll)
+router.get('/get_one', brandController.getOne)
 router.delete('/:id', checkRole('ADMIN'), brandController.delete)
 router.put('/:id', checkRole('ADMIN'), brandController.edit)
 
