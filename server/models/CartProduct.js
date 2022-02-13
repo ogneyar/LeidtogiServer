8eb096fb75d585b28f38ebcce6c880a050abdc6c
ogneyar <ogneyar-ne@ya.ru>
2022-02-13 16:13:39 +0300

    delete excess

--- server/models/CartProduct.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('../db')
-const {DataTypes} = require('sequelize')
-
-
-const CartProduct = sequelize.define('cart_product', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
-})
-
-module.exports = CartProduct
--- server/models/CategoryBrand.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('../db')
-const {DataTypes} = require('sequelize')
-
-
-const CategoryBrand = sequelize.define('category_brand', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
-})
-
-module.exports = CategoryBrand
