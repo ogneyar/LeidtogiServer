17707cbfb0c1956119262c172a08eb7125f728b3
ogneyar <ogneyar-ne@ya.ru>
2022-02-13 16:15:29 +0300

    save just in case

---
+++ server/models/Old/CartProduct.js
@@ -0,0 +1,9 @@
+const sequelize = require('../../db')
+const {DataTypes} = require('sequelize')
+
+
+const CartProduct = sequelize.define('cart_product', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
+})
+
+module.exports = CartProduct
