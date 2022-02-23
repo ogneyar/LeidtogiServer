830e0be6a0bed462a9fc1d482626f6042c95f90b
ogneyar <ogneyar-ne@ya.ru>
2022-02-23 13:14:59 +0300

    мелкие правки

--- dump.bat
+++ dump.bat
@@ -4,6 +4,6 @@ rem скачивание dump'a БД mern
 
 "C:/Program Files/MySQL/MySQL Server 5.5/bin/mysqldump" --host=127.0.0.1 --password= -u root --disable-keys --add-drop-table --default-character-set=utf8 --result-file=mern.sql mern
 
-echo =================SUCCESS========================
+echo =========================================
 
 @pause
