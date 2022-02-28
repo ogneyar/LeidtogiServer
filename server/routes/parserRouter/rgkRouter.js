37b24b9e1457806354a946df8d5fafddff5bcf30
ogneyar <ogneyar-ne@ya.ru>
2022-02-28 19:00:23 +0300

    мелкие правки

--- server/routes/parserRouter/rgkRouter.js
+++ server/routes/parserRouter/rgkRouter.js
@@ -4,8 +4,12 @@ const rgkController = require('../../controllers/parser/rgkController')
 const checkRole = require('../../middleware/checkRoleMiddleware')
 
 
-// router.get('/', rgkController.rgk) // парсер RGK
-router.get('/', checkRole('ADMIN'), rgkController.rgk) // парсер RGK
+
+if (process.env.URL === "http://localhost:5000") {
+    router.get('/', rgkController.rgk) // парсер RGK
+}else {
+    router.get('/', checkRole('ADMIN'), rgkController.rgk) // парсер RGK
+}
 
 // router.get('/add_sizes', rgkController.addSizes) // добавление габаритов RGK
 router.get('/add_sizes', checkRole('ADMIN'), rgkController.addSizes) // добавление габаритов RGK
