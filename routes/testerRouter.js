3e7654d6c0b4ff8aad14c0f04db124d7f6423dfb
ogneyar <ogneyar-ne@ya.ru>
2022-06-07 19:29:24 +0300

    eee

--- routes/testerRouter.js
+++ routes/testerRouter.js
@@ -8,11 +8,12 @@ if (process.env.URL === "http://localhost:5000") {
     router.get('/set_feed', testerController.setFeed)
     router.get('/set_location_cities_sdek', testerController.setLocationCitiesSdek)
     router.get('/set_places_dl', testerController.setPlacesDl)
-    router.get('/temp', testerController.temp)
 }
 router.post('/set_feed', checkRole("ADMIN"), testerController.setFeed)
 router.get('/set_location_cities_sdek', checkRole("ADMIN"), testerController.setLocationCitiesSdek)
 router.get('/set_places_dl', checkRole("ADMIN"), testerController.setPlacesDl)
+
+router.get('/temp', testerController.temp)
 
 
 module.exports = router
