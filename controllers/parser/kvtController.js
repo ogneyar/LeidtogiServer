4cfef29f51aa690a47a84635920790d47016b6d9
ogneyar <ogneyar-ne@ya.ru>
2022-12-06 15:36:43 +0300

    added parse kvt.su for update prices

--- controllers/parser/kvtController.js
+++ controllers/parser/kvtController.js
@@ -1,5 +1,7 @@
 
 const KVT = require('../../service/parser/kvt/KVT')
+const ParseKvtSu = require('../../service/parser/kvt/parseKvtSu')
+const savePriceInFile = require('../../service/savePriceInFile')
 
 
 class kvtController {
@@ -16,11 +18,11 @@ class kvtController {
             if ( ! change ) { // если НЕ смена цен
                 // обработка данных файла feed.xlsx
                 response = await kvt.run(feed)
-                if ( ! response ) return res.json({error: 'Ошибка! ParseXlsx не вернул данные!'}) // вывод ошибки
+                if ( ! response ) return res.json({error: 'Ошибка! Метод run() не вернул данные!'})
             }
             // обработка данных файла price.xlsx
             response = await kvt.run_price(price)
-            if ( ! response ) return res.json({error: 'Ошибка! ParseXlsx не вернул данные!'}) // вывод ошибки
+            if ( ! response ) return res.json({error: 'Ошибка! Метод run_price() не вернул данные!'})
 
             // добавление нового товара
             if (add !== undefined && number) {
@@ -41,9 +43,68 @@ class kvtController {
             return res.json(await kvt.getLength()) 
 
         }catch(e) {
-            return next(res.json({error: 'Ошибка метода kvt! ' + e}))
+            return res.json({error: 'Ошибка метода kvt! ' + e})
         }
     }
+
+
+    async parseKvtSu(req, res, next) {
+        try {
+            let { catalog, numberCatalog, lengthCatalog, products, numberProduct, lengthProducts, prices } = req.query
+            let feed = req.files && req.files.feed || undefined
+
+            let response
+            // создание экземпляра класса KVT
+            let parse = new ParseKvtSu()
+
+            response = await parse.run()
+            if (! response) return res.json({error: 'Ошибка! Метод run() не вернул данные!'})
+
+            if (catalog) { // нет обязательных параметров
+                return res.json(await parse.getCatalog(numberCatalog))
+            }
+            
+            if (lengthCatalog) { 
+                return res.json(await parse.getLengthCatalog())
+            }
+
+            if (products) { // numberCatalog - обязательный параметр
+                return res.json(await parse.getProducts(numberCatalog, numberProduct))
+            }
+
+            if (lengthProducts) { // numberCatalog - обязательный параметр
+                return res.json(await parse.getLengthProducts(numberCatalog))
+            }
+
+            if (prices) { // оба параметра обязательны
+                return res.json(await parse.getPrices(numberCatalog, numberProduct))
+            }
+
+            // по умолчанию (если не задан ни один параметр)
+            return res.json(await parse.getLengthCatalog()) 
+
+        }catch(e) {
+            return res.json({error: 'Ошибка метода parseKvtSu! ' + e})
+        }
+    }
+
+    // сохранение прайса в файл json
+    async savePrice(req, res, next) {
+        try {
+            let { json } = req.body
+            if (! json) {
+                if (req.query && req.query.json) json = req.query.json
+                else throw "Отсутствует тело запроса."
+            }
+
+            savePriceInFile("KVT", json)
+
+            return res.json(json) 
+        }catch(e) {
+            return res.json({error: 'Ошибка метода savePrice! ' + e})
+        }
+    }
+
 
 }
 
--- routes/parserRouter/kvtRouter.js
+++ routes/parserRouter/kvtRouter.js
@@ -5,10 +5,14 @@ const checkRole = require('../../middleware/checkRoleMiddleware')
 
 
 if (process.env.URL === "http://localhost:5000") {
-    router.get('/', kvtController.kvt) // добавление нового товара или обновление цен
-}
-router.post('/', checkRole("ADMIN"), kvtController.kvt) // добавление нового товара или обновление цен
+    router.get('/', kvtController.kvt) // добавление нового товара
+    router.get('/parse', kvtController.parseKvtSu) // обновление цен
+    router.get('/save_price', kvtController.savePrice) // сохранение прайса в файл json
 
+}
+router.post('/', checkRole("ADMIN"), kvtController.kvt) // добавление нового товара
+router.post('/parse', checkRole("ADMIN"), kvtController.parseKvtSu) // обновление цен
+router.post('/save_price', checkRole("ADMIN"), kvtController.savePrice) // сохранение прайса в файл json
 
 
 module.exports = router
