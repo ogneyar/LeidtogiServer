647b79aba9d6e2307ec14618a789ce403fcc253a
ogneyar <ya13th@mail.ru>
2022-12-12 13:07:36 +0300

    eee

--- service/xml/parseXml.js
+++ service/xml/parseXml.js
@@ -1,7 +1,6 @@
 
 const fs = require('fs')
 const convert = require('xml-js')
-const parseHtml = require('../html/parseHtml')
 
 
 async function parseXml(file) {
@@ -10,7 +9,6 @@ async function parseXml(file) {
 
     if (fs.existsSync(file)) {
         xml = fs.readFileSync(file, {encoding:'utf8', flag:'r'})
-        // workbook = await fs.open(file, 'r')
     }else {
         return { error: `Файл ${file} отсутствует или пуст!` }
     }
@@ -18,135 +16,12 @@ async function parseXml(file) {
     xml = xml.replace(/\r|\n/g,"")
     
     
-    var result1 = convert.xml2json(xml, {compact: true, spaces: 0})
-    // console.log(result1)
+    let result = convert.xml2json(xml, {compact: true, spaces: 0})
 
-    return JSON.parse(result1)
-    // return JSON.parse(result1).shop["Разделы"]["Раздел"][07]
-
-
-    //----------------------------------------------------------------------
-    // дальше только тесты
-    //----------------------------------------------------------------------
-
-    try {
-        response = parseHtml(xml,{
-            // entry: `<?xml`,
-            start: `<?xml `,
-            end: `?>`,
-            inclusive: false,
-            return: true
-        })
-    }catch(e) {
-        throw "Не найден заголовок xml файла!"
-    }
-
-    let split = response.search.split(" ")
-    let version, encoding
-    try {
-        version = parseHtml(split[0],{
-            start: `version="`,
-            end: `"`,
-            inclusive: false
-        })
-    }catch(e) {
-        console.log(e)
-    }
-    try {
-        encoding = parseHtml(split[1],{
-            start: `encoding="`,
-            end: `"`,
-            inclusive: false
-        })
-    }catch(e) {
-        console.log(e)
-    }
-    
-    data = reSearch(response.rest.replace(/"/g,"'"))
-
-    return {version, encoding, data}
+    return JSON.parse(result)
 
 }
 
-// рекурсивная функция
-const reSearch = (string) => {
-    
-    // return string
-
-    if (string) string = string.trim()
-    if ( ! string || string === " " ) return ""
-
-    let party, response, result = "", array = [], object = {}
-
-    do {
-
-        try {
-            party = parseHtml(string,{
-                start: `<`,
-                end: `>`,
-                inclusive: false
-            })
-            console.log("party: ",party)
-        }catch(e) {
-            console.log(e)
-            // console.log("string: ",string)
-            return string
-        }
-        
-        try {
-            response = parseHtml(string,{
-                start: `<${party}>`,
-                end: `</${party}>`,
-                inclusive: false, 
-                return: true
-            })
-        }catch(e) {
-            console.log(e)
-            response = {search:""}
-        }
-
-        // return party
-        // return response.rest
-
-        if (response && response.rest && response.rest !== "" && response.rest !== " ") { // значит массив
-
-            // if (party === "shop" || party === "Разделы" || party === "offers") {
-            //     string = response.rest
-            //     result = "[" + result
-            // } else string = ""
-
-            string = response.rest
-            result = "[" + result
-
-        }else { // значит объект
-
-            string = ""
-
-        }
-
-        response = reSearch(response.search)
-
-        // if (party === "Разделы" || party === "offers") result += `{"${party}":"ghghgfh"}`
-        // else result += `{"${party}":${response}}`
-
-        try {
-            result += `{"${party}":${JSON.parse(response)}}`
-        }catch(e) {
-            result += `{"${party}":"${response}"}`
-        }
-        // result += `{"${party}":"${response}"}`
-        if (string) result += ","
-
-        // response = response.rest
-
-    }while(string && string !== "" && string !== " ")
-
-    if (result[0]==="[") result += "]"
-
-    return result
-    // return JSON.parse(result)
-    // return "{" +result +"}"
-}
 
 module.exports = parseXml
 
