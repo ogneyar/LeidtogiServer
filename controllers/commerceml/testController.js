e3ad026134ffc4ac6b6450fdbbce54adc621137e
ogneyar <ya13th@mail.ru>
2023-03-03 13:24:30 +0300

    auto commit

--- controllers/commerceml/testController.js
+++ controllers/commerceml/testController.js
@@ -13,15 +13,17 @@ class TestController {
         let file = req.files && req.files[0] || undefined
 
         if (req.files) {
-            sendMessage("req.files: " + JSON.stringify(req.files), false)
+            await sendMessage("req.files", false)
+            await sendMessage("req.files: " + JSON.stringify(req.files), false)
         }
 
-        if (req.body) {
-            sendMessage("req.body: " + JSON.stringify(req.body), false)
+        if (req.body && req.body !== {}) {
+            await sendMessage("req.body", false)
+            await sendMessage("req.body: " + JSON.stringify(req.body), false)
         }
         
         if (file && file.name !== undefined) {
-            sendMessage("file.name = " + filename)
+            await sendMessage("file.name = " + filename)
             if (!fs.existsSync(path.resolve(__dirname, '..', '..', 'static', 'temp'))) fs.mkdirSync(path.resolve(__dirname, '..', '..', '..', 'static', 'temp'))
             if (!fs.existsSync(path.resolve(__dirname, '..', '..', 'static', 'temp', 'commerceml'))) fs.mkdirSync(path.resolve(__dirname, '..', '..', '..', 'static', 'temp', 'advanta'))
             let fullPath = path.resolve(__dirname, '..', '..', 'static', 'temp', 'commerceml', file.name)
@@ -33,22 +35,22 @@ class TestController {
         if (type !== "catalog") return res.json("success")
 
         if (mode === "checkauth") {
-            sendMessage("checkauth", false)
+            await sendMessage("checkauth", false)
             return res.send(`success\nkuka\n42`)
         }
             
         if (mode === "init") {
-            sendMessage("init", false)
+            await sendMessage("init", false)
             return res.send(`zip=yes\nfile_limit=500`)
         }
             
         if (mode === "file") {
-            sendMessage("mode: " + mode + " filename: " + filename, false)
+            await sendMessage("mode: " + mode + " filename: " + filename, false)
             return res.send(`success`)
         }
             
         if (mode === "import") {
-            sendMessage("mode: " + mode + " filename: " + filename, false)
+            await sendMessage("mode: " + mode + " filename: " + filename, false)
             return res.send(`success`)
         }
 
