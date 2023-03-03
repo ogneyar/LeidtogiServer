f829a25f537787b5dc07b72567747513b0b8103e
Ogneyar <ogneyar@hutoryanin.ru>
2023-03-03 11:29:22 +0000

    eee

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ const CORS_URL = process.env.CORS_URL || "http://leidtogi.ru"
 const CORS_URL_SECURE = process.env.CORS_URL_SECURE || "https://leidtogi.ru"
 
 let whitelist = [
-    CORS_URL, CORS_URL_SECURE,  'http://www.localhost:3000',
+    CORS_URL, CORS_URL_SECURE, 'http://hutor.site/',
+	'http://192.168.178.200:3000', 'http://192.168.178.200:3000/',
+	'http://localhost:3000', 'http://www.localhost:3000',
     'http://www.leidtogi.ru', 'https://www.leidtogi.ru'
 ]
 let corsOptionsDelegate = function (req, callback) {
