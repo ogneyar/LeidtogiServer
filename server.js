f0e90c541f536a75995aa7876ec6712a76cf6534
ogneyar <ya13th@mail.ru>
2023-07-02 10:12:12 +0300

    auto commit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const routerBots = require('./routes/bots/index')
 const routerOthers = require('./routes/others/index')
 const errorHandler = require('./middleware/errorHandlingMiddleware')
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 const CORS_URL = process.env.CORS_URL || "http://leidtogi.ru"
 const CORS_URL_SECURE = process.env.CORS_URL_SECURE || "https://leidtogi.ru"
 
@@ -50,6 +50,8 @@ app.use(fileUpload({}))
 app.use('/api', routerApi)
 app.use('/', routerBots)
 app.use('/', routerOthers)
+// для не существующих роутов
+app.get('*', (req, res) => res.status(404).sendFile(__dirname + '/404.html'))
 
 // Обработка ошибок, последний middleware
 app.use(errorHandler)
