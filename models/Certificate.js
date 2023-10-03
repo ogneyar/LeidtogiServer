4c51b52d109c816a82304b4b04fed4e547cff386
ogneyar <ya13th@mail.ru>
2023-10-03 14:18:17 +0300

    auto commit

--- models/Certificate.js
+++ models/Certificate.js
@@ -5,8 +5,10 @@ const {DataTypes} = require('sequelize')
 const Certificate = sequelize.define('certificate', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     code: {type: DataTypes.STRING, allowNull: false},
-    action: {type: DataTypes.STRING, allowNull: false, defaultValue: "issued"}, // issued, assigned, applied
-    order_id: {type: DataTypes.STRING, defaultValue: null}
+    state: {type: DataTypes.STRING, allowNull: false, defaultValue: "issued"}, // issued, assigned, applied
+    name: {type: DataTypes.STRING, defaultValue: null},
+    url: {type: DataTypes.STRING, defaultValue: null},
+    order_id: {type: DataTypes.INTEGER, defaultValue: null}
 })
 
 module.exports = Certificate
